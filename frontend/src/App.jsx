d4ab650f9a9d76f4f11bc19ef4c20a90be80a798
Nadun Randeera <126862247+Nadunrandeera@users.noreply.github.com>
2025-05-10 11:59:01 +0530

    add MovieCard component and update App to display movies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import MovieCard from './componenets/MovieCard'
 
 function App() {
   return (
     <>
-      <Text display = 'hello' />
-      <Text display = 'bro'/>
+      <MovieCard movie={{Title: "Tom Rider", Poster: "https://via.placeholder.com/150", release_date: "2023-01-01"}} /> 
+      <MovieCard movie={{Title: "Harry potter", Poster: "https://via.placeholder.com/150", release_date: "2005-04-03"}} />
     </>
   )
 }
--- frontend/src/componenets/MovieCard.jsx
+++ frontend/src/componenets/MovieCard.jsx
@@ -1,3 +1,20 @@
-function MovieCars({movie}){
-    
+function MovieCard({movie}){
+
+    function onFavouriteClick() {
+        alert("clicked");
+    }
+
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.Poster} alt={movie.Title} />
+            <div className="movie-overlay">
+                <button className="favourite-btn" onClick={onFavouriteClick}></button> 
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.Title}</h3>
+            <p>{movie.release_date}</p>
+        </div> 
+    </div>
 }
+export default MovieCard;
