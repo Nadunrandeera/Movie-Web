069a78fe94c27f7dbad4b5e9724d57c0745a28db
Nadun Randeera <126862247+Nadunrandeera@users.noreply.github.com>
2025-05-11 22:41:38 +0530

    Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,29 @@
 import MovieCard from "../componenets/MovieCard";
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id:1,Title: "Tom Rider", release_date: "2023-01-01" },
         { id:2,Title: "Harry Potter", release_date: "2005-04-03" },
         { id:3,Title: "Got", release_date: "2010-06-03" },
     ];
+    const handleSearch = (e) => {
+        alert(searchQuery); 
+    }
     return(
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" 
+                placeholder="Search for a movie" 
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+                </form> 
+                
             <div className="movies-grid">
                 {movies.map((movie) => (
                     <MovieCard movie={movie} key={movie.id} />
@@ -16,7 +32,8 @@ function Home() {
             </div>
 
         </div>
-    )
+    );
     
 }
+
 export default Home;
