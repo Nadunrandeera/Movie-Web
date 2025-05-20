3e46b9a817c8d6ff9d0f8a4c77e09022c31fba71
Nadun Randeera <126862247+Nadunrandeera@users.noreply.github.com>
2025-05-20 23:10:30 +0530

    add NavBar component and integrate it into App layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Favourite from './pages/Favourite'
+import Favourite from './pages/favourite'
+import NabVar from './componenets/NavBar'
 
 function App() {
   return (
-    <main className='main-container'> 
+    <div>
+      <NabVar />
+      <main className='main-container'> 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/favourite' element={<Favourite/>} />
       </Routes>
     </main>
+    </div>
+    
   )
 }
 
