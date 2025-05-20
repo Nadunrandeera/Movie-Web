7d6c1e2d8220b8480c374e4b2d0605ef03b2042c
Nadun Randeera <126862247+Nadunrandeera@users.noreply.github.com>
2025-05-20 23:33:29 +0530

    refactor: update file paths for CSS imports and add new styles for components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import './css/App.css'
 import Home from './pages/Home'
 import Favourite from './pages/favourite'
 import NabVar from './componenets/NavBar'
--- frontend/src/componenets/NavBar.jsx
+++ frontend/src/componenets/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import '../css/Navbar.css'
 
 function NabVar(){
     return <nav className="navbar">
