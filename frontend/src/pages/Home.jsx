01a5acda88e85418281ab5007b879692b52fb6f2
Nadun Randeera <126862247+Nadunrandeera@users.noreply.github.com>
2025-05-22 23:28:17 +0530

    feat: integrate movie search functionality and fetch popular movies from API

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import MovieCard from "../componenets/MovieCard";
 import '../css/Home.css'
+import { searchMovies,getPopularMovies } from "../services/api";
 
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
 
-    const movies = [
-        { id:1,Title: "Tom Rider", release_date: "2023-01-01" },
-        { id:2,Title: "Harry Potter", release_date: "2005-04-03" },
-        { id:3,Title: "Got", release_date: "2010-06-03" },
-    ];
+    const movies = getPopularMovies(); 
     const handleSearch = (e) => {
         e.preventDefault();
         alert(searchQuery); //search query
